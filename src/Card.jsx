776d908b490270306d9e0243a06c8644ab75f9d8
Matty <matthewdoaarda@gmail.com>
2024-04-06 00:52:50 +0800

    added cards section

---
+++ src/Card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./styles/Card.css"
+
+const Card = ({children}) => {
+  return (
+    <div className='Card'>{children}</div>
+  )
+}
+
+export default Card
--- src/HomeCards.jsx
+++ src/HomeCards.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { Children } from "react";
+import Card from "./Card.jsx";
+import "./styles/HomeCards.css";
 
 const HomeCards = () => {
   return (
     <section className="HomeCards-Section">
-      <div className="HomeCards-Container"></div>
+      <div className="HomeCards-Container">
+        <Card>
+          <p>
+            For Developers Browse our React jobs and start your career today
+            Browse Jobs For Employers List your job to find the perfect
+            developer for the role Recent Jobs View All Jobs
+          </p>
+        </Card>
+        <Card></Card>
+      </div>
     </section>
   );
 };
