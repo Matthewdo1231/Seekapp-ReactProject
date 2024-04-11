19a2a9b0f4a9b3d385cd7b82d79518545b8d1cc5
matthewdo1231 <matthewdoarda@gmail.com>
2024-04-12 02:48:45 +0800

    route the ViewAllJobButton to /joblisting using useHistory from react-dom

--- src/ViewAllJobs.jsx
+++ src/ViewAllJobs.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./styles/ViewAllJobsButton.css";
+import { useNavigate } from "react-router-dom";
 
 const ViewAllJobs = () => {
+  const navigate = useNavigate();
+
+  const routeToJoblisting = () => {
+    navigate("/joblisting");
+  };
+
   return (
     <div className="ViewAllJobsButton-Container">
-      <button className="ViewAllJobsButton">ViewAllJobs</button>
+      <button onClick={routeToJoblisting} className="ViewAllJobsButton">
+        ViewAllJobs
+      </button>
     </div>
   );
 };
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -11,8 +11,8 @@ const Homepage = () => {
       <Hero />
       <HomeCards />
       <RecentJobs />
-      <ViewAllJobs />
       <JobListings />
+      <ViewAllJobs />
     </>
   );
 };
